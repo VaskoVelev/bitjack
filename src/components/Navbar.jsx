90e4c92178b2ddf9bf125436d5ad77fd8c8a0c25
Vasil Velev <vasilvelev07@gmail.com>
2025-06-03 20:18:51 +0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import "../styles/Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="bitjack-navbar">
+      <div className="bitjack-logo">BitJack</div>
+    </nav>
+  );
+}
