bfc6360f3f6328081ca0e7e465b26203a22b50dc
Vasko1234 <vasetovelev07@gmail.com>
2025-06-19 10:58:03 +0300

    main reset

--- src/components/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/Auth.css";
-
-export default function AuthLayout({ children }) {
-  return (
-    <div className="auth-container">
-      <div className="auth-overlay auth-card">{children}</div>
-    </div>
-  );
-}
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/Navbar.css";
-
-export default function Navbar() {
-  return (
-    <nav className="bitjack-navbar">
-      <div className="bitjack-logo">BitJack</div>
-    </nav>
-  );
-}
